553437622a601a54f4f3be153f3f78a2ef81e98e
DarrenInwood <darren.inwood@gmail.com>
2016-06-26 23:22:14 +1200

    Main loom - straight arduino version

--- main-loom/main-loom.ino
+++ main-loom/main-loom.ino
@@ -1,5 +1,3 @@
-#include <SCoop.h>
-
 // --------------------------------------------------------------------
 // Physical hardware
 // --------------------------------------------------------------------
@@ -13,77 +11,139 @@
 Vehicle vehicle;
 
 // --------------------------------------------------------------------
-// CAN bus interface
+// CAN bus communications
 // --------------------------------------------------------------------
-#define CAN_MSG_ID    0x07
-
+#include <CAN.h>
 #include <SPI.h>
-#include <mcp_can.h>
 
-#define CAN_CS_PIN    4
-#define CAN_INT_PIN   2
-MCP_CAN CAN(CAN_CS_PIN);
+#define CAN_MSG_HEADER  0x02DA
+#define CAN_TYPE_SWITCH 0x00
+#define CAN_TYPE_VALUE  0x01
 
-#define CAN_TX_QUEUE_LEN  8
-defineFifo(canTxQueue, uint32_t, CAN_TX_QUEUE_LEN);
-
-#define CAN_RX_QUEUE_LEN  8
-defineFifo(canRxQueue, uint32_t, CAN_RX_QUEUE_LEN);
-
-// Flag used to indicate that there is something to read
-// on the CAN bus
-volatile bool canRxSema = false;
-
-// Used to convert messages between various integer formats
-#define CAN_TYPE_SWITCH   0x00
-#define CAN_TYPE_VALUE    0x01
-union CanFrame
+// Allows us to easily convert between the CAN message ID
+// and the intent of the message
+union CanMessageId
 {
    uint8_t data[4];
    uint32_t full;
    struct {
-      uint8_t type;
       uint8_t index;
-      uint16_t val;
+      uint8_t type;
+      uint16_t header;
    } parts;
 };
-CanFrame buf;
 
-/*
- * Sets up the interrupt to trigger reading incoming CAN messages.
- */
-void setupCanbus()
+// Allows us to easily convert between various interpretations
+// of values sent via CAN bus
+union CanData
 {
-  // Interrupt on pin D2 triggere ISR
-  attachInterrupt(
-    digitalPinToInterrupt(CAN_INT_PIN),
-    canInterrupt,
-    LOW
-  );
-  // Start the CAN bus at 100Kbps
-  CAN.begin(CAN_100KBPS);
+   uint8_t data[4];
+   uint32_t full;
+};
+
+void setupCanbus(void)
+{
+  Serial.begin(115200);  
+  CAN.begin(CAN_BPS_100K);
 }
 
 /*
  * Add a switch type CAN message to the transmit queue
  */
-void queueCanSwitch(uint8_t index)
+void queueCanMessage(uint8_t swtype, uint8_t index)
 {
-  buf.parts.type = CAN_TYPE_SWITCH;
-  buf.parts.index = index;
-  buf.parts.val = (uint16_t)vehicle.switches[index];
-  canTxQueue.put(&(buf.full));
+  static CAN_Frame can_message;
+  CanMessageId messageId;
+  CanData messageData;
+
+  // Encode the CAN message ID
+  messageId.parts.header = CAN_MSG_HEADER;
+  messageId.parts.type = swtype;
+  messageId.parts.index = index;
+
+  can_message.id = messageId.full;
+  can_message.valid = true;
+  can_message.rtr = 0;
+  can_message.extended = CAN_EXTENDED_FRAME;
+  can_message.length = 2;
+
+  switch (swtype) {
+    case CAN_TYPE_SWITCH:
+      messageData.data[0] = 0x00;
+      messageData.data[1] = 0x00;
+      messageData.data[2] = 0x00;
+      messageData.data[3] = (uint8_t)vehicle.switches[index];
+    break;
+    case CAN_TYPE_VALUE:
+      messageData.full = (uint32_t)vehicle.switches[index];
+    break;
+  }
+  can_message.data[0] = messageData.data[0];
+  can_message.data[1] = messageData.data[1];
+  can_message.data[2] = messageData.data[2];
+  can_message.data[3] = messageData.data[3];
+  
+  CAN.write(can_message);
+
+  Serial.print(millis());
+  Serial.print(F(",0x"));
+  Serial.print(can_message.id, HEX); //display message ID
+  Serial.print(',');
+  Serial.print(can_message.rtr); //display message RTR
+  Serial.print(',');
+  Serial.print(can_message.extended); //display message EID
+  Serial.print(',');
+  Serial.print(can_message.data[0], HEX);
+  Serial.print(can_message.data[1], HEX);
+  Serial.print(can_message.data[2], HEX);
+  Serial.println(can_message.data[3], HEX);
 }
 
-/*
- * Add a value type CAN message to the transmit queue
+/* 
+ * Fetch and process any waiting receieved CAN messages.
+ * Ensure this is called every 1ms so as not to lose packets.
  */
-void queueCanValue(uint8_t index)
+void processCanMessage()
 {
-  buf.parts.type = CAN_TYPE_VALUE;
-  buf.parts.index = index;
-  buf.parts.val = vehicle.values[index];
-  canTxQueue.put(&(buf.full));
+  static CAN_Frame message; // Create message object to use CAN message structure
+  CanMessageId messageId;
+  CanData messageData;
+  if (CAN.available() != true) // Check to see if a valid message has been received.
+  {
+    return; // noop
+  }
+  message = CAN.read();
+  if (message.valid != 0) {
+    return; // discard invalid messages 
+  }
+  if (message.extended != CAN_EXTENDED_FRAME) {
+    return; // discard standard frame size messages
+  }
+  messageId.full = message.id;
+  messageData.data[0] = message.data[0];
+  messageData.data[1] = message.data[1];
+  messageData.data[2] = message.data[2];
+  messageData.data[3] = message.data[3];
+  if (message.rtr == 1) {
+    // We've requested a value
+    queueCanMessage(messageId.parts.type, messageId.parts.index);
+    return;
+  }
+  switch (messageId.parts.header) {
+    // Normal message
+    case CAN_MSG_HEADER:
+      switch (messageId.parts.type)
+      {
+        case CAN_TYPE_SWITCH:
+          vehicle.switches[messageId.parts.index] = (messageData.data[3] == 0x01);
+        break;
+        case CAN_TYPE_VALUE:
+          vehicle.values[messageId.parts.index] = messageData.full;
+        break;
+      }
+    break;
+    // No default - other messages are discarded
+  }
 }
 
 // --------------------------------------------------------------------
@@ -115,74 +175,10 @@ void setupInputs() {
   pinMode(PIN_PARKLIGHTS, INPUT); // D10 <- Park lights
 }
 
-// --------------------------------------------------------------------
-// ISRs
-// --------------------------------------------------------------------
-
-/*
- * ISR:  Pin change on pin D2 (INT0) means CAN interrupt. Signal
- * the CAN receive thread to process it.
- */
-void canInterrupt()
-{
-  // Tell the read task to read.
-  canRxSema = true;
-}
-
-// --------------------------------------------------------------------
-// Tasks
-// --------------------------------------------------------------------
-
-/*
- * TASK:  When there's a CAN message waiting to be read, read it
- * and add it to the receieve queue.
- */
-uint8_t len;
-defineTaskLoop(readCanMessage, 64)
-{
-  // Wait until the semaphore is set
-  // (ie. when the interrupt handler has set it)
-  sleepUntil(canRxSema);
-  if (CAN_MSGAVAIL == CAN.checkReceive()) {
-    CAN.readMsgBuf(&len, buf.data);
-    canRxQueue.put(&(buf.full));
-  }
-}
-
-/*
- * TASK:  When there's a message in the CAN receive buffer, process it.
- */
-defineTaskLoop(processCanRxQueue, 64)
-{
-  if (canRxQueue.get(&(buf.full))) {
-    // Set the new state on the vehicle state
-    switch (buf.parts.type) {
-       case 0:
-           // Don't send a can message if we changed state
-           vehicle.setSwitch(buf.parts.index, buf.parts.val);
-       break;
-       case 1:
-           // Don't send a can message if we changed state
-           vehicle.setValue(buf.parts.index, buf.parts.val);
-       break;
-    }
-  }
-}
-
-/*
- * TASK:  When there's a message in the CAN transmit buffer, send it.
- */
-defineTaskLoop(processCanTxQueue, 64)
-{
-  if (canTxQueue.get(&(buf.full))) {
-    CAN.sendMsgBuf(CAN_MSG_ID, 0, sizeof(buf.data), buf.data);
-  }
-}
-
 /*
  * TASK:  Debounce physical inputs.
  */
-defineTaskLoop(processInputs, 64)
+void debounceInputs()
 {
   // Debounce D8/D9/D10
   debouncePB.ButtonProcess(PINB);
@@ -192,17 +188,16 @@ defineTaskLoop(processInputs, 64)
   debouncePCF1.ButtonProcess(expander1.read8());
   // Debounce PCF8574 #2
   debouncePCF2.ButtonProcess(expander2.read8());
-  // Debounce every 10ms
-  sleep(10);
 }
 
 /*
  * TASK:  Changes in debounced inputs map to changes in vehicle state.
  */
-volatile bool debouncedValue;
-volatile bool debouncedValue2;
-defineTaskLoop(copyInputsToState, 128)
+void processInputs()
 {
+  bool debouncedValue;
+  bool debouncedValue2;
+
   debouncedValue = debouncePD.ButtonCurrent(DIN_TURNLEFT);
   debouncedValue2 = debouncePD.ButtonCurrent(DIN_TURNRIGHT);
 
@@ -210,50 +205,50 @@ defineTaskLoop(copyInputsToState, 128)
   if (debouncedValue && debouncedValue2) {
     if (!vehicle.switches[SW_HAZARD]) {
       vehicle.setSwitch(SW_HAZARD, true);
-      queueCanSwitch(SW_HAZARD);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_HAZARD);
     }
     // If Hazards go on, L&R go off
     if (vehicle.switches[SW_TURNLEFT]) {
       vehicle.setSwitch(SW_TURNLEFT, false);
-      queueCanSwitch(SW_TURNLEFT);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_TURNLEFT);
     }
     if (vehicle.switches[SW_TURNRIGHT]) {
       vehicle.setSwitch(SW_TURNRIGHT, false);
-      queueCanSwitch(SW_TURNRIGHT);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_TURNRIGHT);
     }
   } else {
     // Hazards go off
     if (vehicle.switches[SW_HAZARD]) {
       vehicle.setSwitch(SW_HAZARD, false);
-      queueCanSwitch(SW_HAZARD);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_HAZARD);
     }
     // Check L&R inputs
     if (vehicle.switches[SW_TURNLEFT] != debouncedValue) {
       vehicle.setSwitch(SW_TURNLEFT, debouncedValue);
-      queueCanSwitch(SW_TURNLEFT);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_TURNLEFT);
     }
     if (vehicle.switches[SW_TURNRIGHT] != debouncedValue2) {
       vehicle.setSwitch(SW_TURNRIGHT, debouncedValue2);
-      queueCanSwitch(SW_TURNRIGHT);
+      queueCanMessage(CAN_TYPE_SWITCH, SW_TURNRIGHT);
     }
   }
 
   debouncedValue = !debouncePB.ButtonCurrent(DIN_ACC);
   if (vehicle.switches[SW_ACC] != debouncedValue) {
     vehicle.setSwitch(SW_ACC, debouncedValue);
-    queueCanSwitch(SW_ACC);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_ACC);
   }
 
   debouncedValue = !debouncePB.ButtonCurrent(DIN_IGNITION);
   if (vehicle.switches[SW_IGNITION] != debouncedValue) {
     vehicle.setSwitch(SW_IGNITION, debouncedValue);
-    queueCanSwitch(SW_IGNITION);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_IGNITION);
   }
 
   debouncedValue = !debouncePB.ButtonCurrent(DIN_PARKLIGHTS);
   if (vehicle.switches[SW_PARKLIGHTS] != debouncedValue) {
     vehicle.setSwitch(SW_PARKLIGHTS, debouncedValue);
-    queueCanSwitch(SW_PARKLIGHTS);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_PARKLIGHTS);
   }
 
   // PCF8574 #1 inputs
@@ -261,49 +256,49 @@ defineTaskLoop(copyInputsToState, 128)
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_BRAKESWITCH);
   if (vehicle.switches[SW_BRAKE] != debouncedValue) {
     vehicle.setSwitch(SW_BRAKE, debouncedValue);
-    queueCanSwitch(SW_BRAKE);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_BRAKE);
   }
 
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_FUELLOW);
   if (vehicle.switches[SW_WARN_FUEL] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_FUEL, debouncedValue);
-    queueCanSwitch(SW_WARN_FUEL);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_FUEL);
   }
 
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_CHOKE);
   if (vehicle.switches[SW_CHOKE] != debouncedValue) {
     vehicle.setSwitch(SW_CHOKE, debouncedValue);
-    queueCanSwitch(SW_CHOKE);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_CHOKE);
   }
 
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_COOLANTLOW);
   if (vehicle.switches[SW_WARN_COOLANTLEVEL] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_COOLANTLEVEL, debouncedValue);
-    queueCanSwitch(SW_WARN_COOLANTLEVEL);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_COOLANTLEVEL);
   }
 
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_OILPRESSURELOW);
   if (vehicle.switches[SW_WARN_OILPRESSURE] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_OILPRESSURE, debouncedValue);
-    queueCanSwitch(SW_WARN_OILPRESSURE);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_OILPRESSURE);
   }
 
   debouncedValue = !debouncePCF1.ButtonCurrent(DIN_TURNLEFTLIGHT); // invert
   if (vehicle.switches[SW_WARN_TURNLEFT] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_TURNLEFT, debouncedValue);
-    queueCanSwitch(SW_WARN_TURNLEFT);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_TURNLEFT);
   }
 
   debouncedValue = !debouncePCF1.ButtonCurrent(DIN_TURNRIGHTLIGHT); // invert
   if (vehicle.switches[SW_WARN_TURNRIGHT] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_TURNRIGHT, debouncedValue);
-    queueCanSwitch(SW_WARN_TURNRIGHT);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_TURNRIGHT);
   }
 
   debouncedValue = debouncePCF1.ButtonCurrent(DIN_WIPERINT);
   if (vehicle.switches[SW_WIPERS_INT] != debouncedValue) {
     vehicle.setSwitch(SW_WIPERS_INT, debouncedValue);
-    queueCanSwitch(SW_WIPERS_INT);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WIPERS_INT);
   }
 
   // PCF8574 #2 inputs
@@ -311,54 +306,51 @@ defineTaskLoop(copyInputsToState, 128)
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_WASHERMOTOR);
   if (vehicle.switches[SW_WASHER] != debouncedValue) {
     vehicle.setSwitch(SW_WASHER, debouncedValue);
-    queueCanSwitch(SW_WASHER);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WASHER);
   }
 
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_WIPERLOW);
   if (vehicle.switches[SW_WIPERS_LOW] != debouncedValue) {
     vehicle.setSwitch(SW_WIPERS_LOW, debouncedValue);
-    queueCanSwitch(SW_WIPERS_LOW);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WIPERS_LOW);
   }
 
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_WIPERHIGH);
   if (vehicle.switches[SW_WIPERS_HIGH] != debouncedValue) {
     vehicle.setSwitch(SW_WIPERS_HIGH, debouncedValue);
-    queueCanSwitch(SW_WIPERS_HIGH);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WIPERS_HIGH);
   }
 
   // Not sure what this pin does!
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_UNKNOWN);
   if (vehicle.switches[SW_UNKNOWN] != debouncedValue) {
     vehicle.setSwitch(SW_UNKNOWN, debouncedValue);
-    queueCanSwitch(SW_UNKNOWN);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_UNKNOWN);
   }
 
   debouncedValue = !debouncePCF2.ButtonCurrent(DIN_HEADLIGHTS);
   if (vehicle.switches[SW_HEADLIGHTS] != debouncedValue) {
     vehicle.setSwitch(SW_HEADLIGHTS, debouncedValue);
-    queueCanSwitch(SW_HEADLIGHTS);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_HEADLIGHTS);
   }
 
   debouncedValue = !debouncePCF2.ButtonCurrent(DIN_HIGHBEAM);
   if (vehicle.switches[SW_HIGHBEAM] != debouncedValue) {
     vehicle.setSwitch(SW_HIGHBEAM, debouncedValue);
-    queueCanSwitch(SW_HIGHBEAM);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_HIGHBEAM);
   }
 
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_HANDBRAKE);
   if (vehicle.switches[SW_HANDBRAKE] != debouncedValue) {
     vehicle.setSwitch(SW_HANDBRAKE, debouncedValue);
-    queueCanSwitch(SW_HANDBRAKE);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_HANDBRAKE);
   }
 
   debouncedValue = debouncePCF2.ButtonCurrent(DIN_BRAKEFLUIDLOW);
   if (vehicle.switches[SW_WARN_BRAKEFLUID] != debouncedValue) {
     vehicle.setSwitch(SW_WARN_BRAKEFLUID, debouncedValue);
-    queueCanSwitch(SW_WARN_BRAKEFLUID);
+    queueCanMessage(CAN_TYPE_SWITCH, SW_WARN_BRAKEFLUID);
   }
-
-  // Sleep for 10ms
-  sleep(10);
 }
 
 // --------------------------------------------------------------------
@@ -369,12 +361,13 @@ void setup()
 {
   setupInputs();
   setupCanbus();
-
-  mySCoop.start();
 }
 
 void loop()
 {
-  // Empty  
+  processCanMessage();
+  debounceInputs();
+  processInputs();
+  delay(10);
 }
 
